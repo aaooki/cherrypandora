a76667085ebd231691330fcaf8918d25bb8baed9
aonemd <aonemdsaleh@gmail.com>
2016-12-15 02:35:21 +0200

    WIP: Add Twitter auth handlers

--- lib/cherry_pandora/app.rb
+++ lib/cherry_pandora/app.rb
@@ -5,5 +5,43 @@ module CherryPandora
     get '/' do
       erb :timer
     end
+
+    # Twitter auth
+    use OmniAuth::Builder do
+      provider :twitter, 'key', 'secret'
+    end
+
+    configure do
+      enable :sessions
+    end
+
+    helpers do
+      def current_user?
+        session[:user]
+      end
+    end
+
+    get '/public' do
+      'public'
+    end
+
+    get '/private' do
+      halt(401, 'Unauthorized') unless current_user?
+      'private'
+    end
+
+    get '/login' do
+      redirect to("/auth/twitter")
+    end
+
+    get '/auth/twitter/callback' do
+      session[:user] = true
+      env['omniauth.auth']
+    end
+
+    get '/logout' do
+      session[:user] = nil
+      "Logged out"
+    end
   end
 end
