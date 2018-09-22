8b128d64fa2893b8bc1d4ee008b02be1c5dc2496
aonemd <aonemdsaleh@gmail.com>
2018-09-22 15:33:16 +0200

    Revert "WIP: Comment out the app for now"
    This reverts commit e92476cacaa543cca38ce3776f3d95c01ab6a24a.

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ module Panadoura
   class Application < Sinatra::Base
     register Sinatra::Namespace
 
-    # use AssetsHelper
-    # use SessionsHelper
-    #
-    # helpers SessionsHelper::UserSession
+    use AssetsHelper
+    use SessionsHelper
+
+    helpers SessionsHelper::UserSession
 
     namespace '/api' do
       get '' do
@@ -17,68 +17,61 @@ module Panadoura
       end
     end
 
-    get '/' do
-        content_type :json
+    # Twitter auth
+    use OmniAuth::Builder do
+      provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+    end
 
-        { msg: "Hello, World!" }.to_json
+    get '/' do
+      erb :timer
+    end
+
+    get '/login' do
+      redirect to("/auth/twitter")
+    end
+
+    get '/auth/twitter/callback' do
+      response = env['omniauth.auth']
+
+      if response
+        user_repo = ROMConfig.new.repository(UserRepository)
+        user = user_repo.by_uid(response.uid)
+        unless user
+          user = user_repo.create(username: response.info.nickname, uid: response.uid)
+        end
+
+        session[:user] = user.to_h
+        redirect to '/'
+      else
+        halt(401, 'Unauthorized')
       end
     end
 
-    # # Twitter auth
-    # use OmniAuth::Builder do
-    #   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-    # end
-    #
-    # get '/' do
-    #   erb :timer
-    # end
-    #
-    # get '/login' do
-    #   redirect to("/auth/twitter")
-    # end
-    #
-    # get '/auth/twitter/callback' do
-    #   response = env['omniauth.auth']
-    #
-    #   if response
-    #     user_repo = ROMConfig.new.repository(UserRepository)
-    #     user = user_repo.by_uid(response.uid)
-    #     unless user
-    #       user = user_repo.create(username: response.info.nickname, uid: response.uid)
-    #     end
-    #
-    #     session[:user] = user.to_h
-    #     redirect to '/'
-    #   else
-    #     halt(401, 'Unauthorized')
-    #   end
-    # end
-    #
-    # get '/logout' do
-    #   logout!
-    #   redirect to '/'
-    # end
-    #
-    # get '/tracker' do
-    #   if logged_in?
-    #     entry_repo = ROMConfig.new.repository(EntryRepository)
-    #     @entries = entry_repo.by_user_id(current_user[:id])
-    #
-    #     erb :tracker
-    #   else
-    #     halt(401, 'Unauthorized')
-    #   end
-    # end
-    #
-    # post '/entry' do
-    #   if logged_in?
-    #     entry_repo = ROMConfig.new.repository(EntryRepository)
-    #     new_entry = entry_repo.create(length: params[:length], created_at: DateTime.now, user_id: current_user[:id])
-    #
-    #     halt(200)
-    #   else
-    #     halt(401, 'Unauthorized')
-    #   end
-    # end
+    get '/logout' do
+      logout!
+      redirect to '/'
+    end
+
+    get '/tracker' do
+      if logged_in?
+        entry_repo = ROMConfig.new.repository(EntryRepository)
+        @entries = entry_repo.by_user_id(current_user[:id])
+
+        erb :tracker
+      else
+        halt(401, 'Unauthorized')
+      end
+    end
+
+    post '/entry' do
+      if logged_in?
+        entry_repo = ROMConfig.new.repository(EntryRepository)
+        new_entry = entry_repo.create(length: params[:length], created_at: DateTime.now, user_id: current_user[:id])
+
+        halt(200)
+      else
+        halt(401, 'Unauthorized')
+      end
+    end
   end
 end
