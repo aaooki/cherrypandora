54ce017d8727d3d7a00b6246e4c42ac6269feb09
aonemd <aonemdsaleh@gmail.com>
2018-10-25 00:20:46 +0200

    Wrap RequestAuthenticator under Panadoura module to fix file requires

--- lib/panadoura/services/request_authenticator.rb
+++ lib/panadoura/services/request_authenticator.rb
@@ -1,34 +1,36 @@
-class RequestAuthenticator
-  def initialize(request)
-    @request = request
-  end
+module Panadoura
+  class RequestAuthenticator
+    def initialize(request)
+      @request = request
+    end
 
-  def call
-    valid_header? && valid_payload?
-  end
+    def call
+      valid_header? && valid_payload?
+    end
 
-  private
+    private
 
-  def valid_header?
-    !!@request.env.fetch('HTTP_AUTHORIZATION', '').scan(/Bearer/).flatten.first
-  end
+    def valid_header?
+      !!@request.env.fetch('HTTP_AUTHORIZATION', '').scan(/Bearer/).flatten.first
+    end
 
-  def valid_payload?
-    payload && !JWTEncoderDecoder.expired?(payload) && valid_user?
-  end
+    def valid_payload?
+      payload && !JWTEncoderDecoder.expired?(payload) && valid_user?
+    end
 
-  def valid_user?
-    user_repo = ROMConfig.new.repository(UserRepository)
-    user      = user_repo.by_uid(response.uid)
+    def valid_user?
+      user_repo = ROMConfig.new.repository(UserRepository)
+      user      = user_repo.by_uid(response.uid)
 
-    !!user
-  end
+      !!user
+    end
 
-  def payload
-    @payload ||= JWTEncoderDecoder.decode(access_token)
-  end
+    def payload
+      @payload ||= JWTEncoderDecoder.decode(access_token)
+    end
 
-  def access_token
-    @request.env.fetch('HTTP_AUTHORIZATION', '').scan(/Bearer (.*)$/).flatten.last
+    def access_token
+      @request.env.fetch('HTTP_AUTHORIZATION', '').scan(/Bearer (.*)$/).flatten.last
+    end
   end
 end
