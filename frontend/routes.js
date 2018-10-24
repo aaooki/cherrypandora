bc408e93fa4ffa34d0c2869dfc3beb52bfad088c
aonemd <aonemdsaleh@gmail.com>
2018-10-24 17:22:41 +0200

    Add Authentication component
    It acts as a middleware between the backend and the frontend.

--- frontend/routes.js
+++ frontend/routes.js
@@ -1,7 +1,10 @@
 import Timer from './components/Timer.vue';
 import LogIn from './components/LogIn.vue';
+import Authentication from './components/Authentication.vue';
 
 export default [
   { path: '/', component: Timer },
-  { path: '/login', component: LogIn }
+  { path: '/login', component: LogIn },
+  { path: '/authenticate', component: Authentication },
+  { path: '*', component: Timer }
 ]
