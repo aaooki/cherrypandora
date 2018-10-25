196425d14e41b2781f49de3fed8876916089180d
aonemd <aonemdsaleh@gmail.com>
2018-10-25 19:30:58 +0200

    Wrap JWTEncoderDecoder under Panadoura module

--- lib/panadoura/jwt_encoder_decoder.rb
+++ lib/panadoura/jwt_encoder_decoder.rb
@@ -1,19 +1,21 @@
 require 'date'
 
-module JWTEncoderDecoder
-  def self.encode(payload, expires_in: DateTime.now.next_month.strftime('%s'))
-    payload[:exp] = expires_in
+module Panadoura
+  module JWTEncoderDecoder
+    def self.encode(payload, expires_in: DateTime.now.next_month.strftime('%s'))
+      payload[:exp] = expires_in
 
-    JWT.encode(payload, ENV['SESSION_SECRET'])
-  end
+      JWT.encode(payload, ENV['SESSION_SECRET'])
+    end
 
-  def self.decode(token)
-    JWT.decode(token, ENV['SESSION_SECRET']).first
-  rescue
-    nil
-  end
+    def self.decode(token)
+      JWT.decode(token, ENV['SESSION_SECRET']).first
+    rescue
+      nil
+    end
 
-  def self.expired?(payload)
-    DateTime.strptime(payload['exp'], '%s') < DateTime.now
+    def self.expired?(payload)
+      DateTime.strptime(payload['exp'], '%s') < DateTime.now
+    end
   end
 end
