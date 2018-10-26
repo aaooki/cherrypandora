27a46f9bc30c93e4162ed5882584486e2570c8df
aonemd <aonemdsaleh@gmail.com>
2018-10-26 22:17:59 +0200

    Properly require files for test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,12 @@
 ENV['RACK_ENV'] = 'test'
 
 require 'minitest/autorun'
+require 'rack/test'
 require 'bundler'
 Bundler.require :default, ENV['RACK_ENV']
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require File.expand_path 'lib/panadoura'
+require File.expand_path 'app/application'
 
 class TestHelper < MiniTest::Test
   include Rack::Test::Methods
